26829805efe18b347a3fa549be4b2a77f4eb1598
jsu-on-the-web <janet.suryadi@nearmap.com>
2025-04-30 16:42:48 +1000

    Adding a resize handle

--- front-end/src/components/Modal/index.tsx
+++ front-end/src/components/Modal/index.tsx
@@ -22,12 +22,13 @@ interface ModalProps {
 const Modal = ({ children, height, width, onClose }: ModalProps) => {
     return ( 
         <div className="w-1/6 modal h-1/3" onClick={onClose}>
-            <div className="modal__content" style={{ height: height, width: width }} onClick={(e) => e.stopPropagation()}>
+            <article className="modal__content" style={{ height: height, width: width }} onClick={(e) => e.stopPropagation()}>
                 <button className="modal__close-button" onClick={onClose}>
                     <FontAwesomeIcon icon={faXmark} />
                 </button>
                 {children}
-            </div>
+                <div className='modal__resize-handle' />
+            </article>
         </div>
     );
  };
