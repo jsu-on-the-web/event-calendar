0b427f811c988947959fe33d449525a83249c9b1
jsu-on-the-web <janet.suryadi@nearmap.com>
2025-05-02 00:32:36 +1000

    Removing test button

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,7 +4,7 @@ import Modal from './components/Modal'
 import { useModal } from './contexts/ModalContext';
 
 function App() {
-  const { showModal, openModal, closeModal } = useModal();
+  const { showModal, closeModal } = useModal();
   return (
     <>
       {/* <body>
@@ -12,13 +12,6 @@ function App() {
       </body> */}
         <Calendar />
         {showModal && (<Modal children={<p>Hello world!</p>} onClose={closeModal} />)}
-
-        <button
-          className="px-4 py-2 font-semibold text-black bg-blue-500 rounded open-modal-button hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
-          onClick={openModal}
-        >
-          Test Button
-        </button>
     </>
   )
 }
